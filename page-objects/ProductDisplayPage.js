ea7e2bbb0358823c66bdbef19ed4d09393dec1ba
Jake Jarosz <jacob.jarosz@dcsg.com>
2024-05-28 16:48:14 -0400

    Revert "New PDP Smoke TC for CI/CD Pipeline on Playwright fwk"

--- page-objects/ProductDisplayPage.js
+++ page-objects/ProductDisplayPage.js
@@ -4,18 +4,9 @@ export class ProductDisplayPage {
 
         this.storePickupEnabledButton = page.getByRole('button', { name: 'Free Store Pickup In stock at' })
         this.storePickupSubText = page.locator('#pdp-in-store-pickup-subtext div')
-        this.addToCartButton = page.getByRole('button', { name: 'ADD TO CART', exact: true })
-        this.firstColorAvailable = page.locator('(//button//div[contains(@class, "color-swatch-wrapper")])[1]')
-        this.goToCartButton = page.locator('.atc-cart-link')
-        this.pleaseSelectColor = page.getByText('Please Select Color')
-        this.addedToCartMessage = page.getByText('ADDED TO CART')
-        this.continueShoppingMessage = page.getByText('Continue Shopping')
-        this.goToCartMessage = page.getByText('GO TO CART')
-        this.oneItemCart = page.getByText('1 item')
-        this.shipToMeFullfilmentButton = page.getByRole('button', { name: 'Ship' }).getByText('Available')
-        this.freeStorePickupButton = page.locator('pdp-feature-in-store-pickup-button')
-        this.changeStoreButton = page.getByRole('button', { name: 'Change Store' })
-        this.storesWithAvailabilityCheckbox = page.getByText('All Stores w/ Availability')
+        this.addToCartButton = page.locator('#pdp-add-to-cart-button')
+        this.goToCartButton = page.getByRole('link', { name: 'GO TO CART' })
+
     }
 }
 
