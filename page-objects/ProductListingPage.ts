bb849731d3170d4f2ab5987c4ba7e8480f77c428
Sean Clawson <sean.clawson@dcsg.com>
2024-06-26 11:57:48 -0400

    added test for srlp ship to home

--- page-objects/ProductListingPage.ts
+++ page-objects/ProductListingPage.ts
@@ -5,14 +5,53 @@ import { getIndexThatIncludesFirstMatch } from '../lib/functions';
 
 export class ProductListingPage {
     private page: Page;
-    
+
     readonly changeSelectedStoreLink: Locator;
     readonly selectStoreZipField: Locator;
     readonly selectStoreSearchButton: Locator;
     readonly selectStoreNames: Locator;
     readonly selectStoreButtons: Locator;
     readonly pickupFilterButton: Locator;
+    readonly shipFilterButtonReact: Locator;
+    readonly shipFilterButtonAngular: Locator;
+    readonly saleAccordionFilterButtonReact: Locator;
+    readonly saleAccordionFilterButtonAngular: Locator;
+    readonly saleFilterValueReact: Locator;
+    readonly saleFilterValueAngular: Locator;
+    readonly sizeAccordionFilterButtonReact: Locator;
+    readonly sizeAccordionFilterButtonAngular: Locator;
+    readonly sizeFilterValueReact: Locator;
+    readonly sizeFilterValueAngular: Locator;
+    readonly filterChipsReact: Locator;
+    readonly filterChipsAngular: Locator;
     readonly productNames: Locator;
+    readonly productNamesAngular: Locator;
+    readonly productPromotionsReact: Locator;
+    readonly productPromotionsAngular: Locator;
+    readonly productAvailabilityReact: Locator;
+    readonly productAvailabilityAngular: Locator;
+    readonly productPriceReact: Locator;
+    readonly productPriceAngular: Locator;
+    readonly zipDeliveryLocationButton: Locator;
+    readonly zipDeliveryInputField: Locator;
+    readonly zipDeliveryUpdateButton: Locator;
+    readonly rightChevronNextButtonReact: Locator;
+    readonly rightChevronNextButtonAngular: Locator;
+    readonly highlightedPageNumberReact: Locator;
+    readonly highlightedPageNumberAngular: Locator;
+    readonly sortOptionsAccordionButtonReact: Locator;
+    readonly sortOptionsAccordionButtonAngular: Locator;
+    readonly sortOptionsSelectionReact: Locator;
+    readonly sortOptionsSelectionAngular: Locator;
+    readonly sortSelectedReact: Locator;
+    readonly sortSelectedAngular: Locator;
+    readonly quickviewOpenATCButtonReact: Locator;
+    readonly quickviewOpenATCButtonAngular: Locator;
+    readonly quickviewColorAttribute: Locator;
+    readonly quickviewSizeAttribute: Locator;
+    readonly quickviewModalATCButton: Locator;
+    readonly quickviewKeepShoppingButton: Locator;
+    readonly quickviewViewCartButton: Locator;
 
     constructor(page: Page) {
         this.page = page;
@@ -24,8 +63,61 @@ export class ProductListingPage {
         this.selectStoreNames = page.locator('[class="hmf-text-transform-capitalize"]');
         this.selectStoreButtons = page.getByLabel('select store');
 
+        // zip delivery location
+        this.zipDeliveryLocationButton = page.getByLabel(new RegExp('.*Zip Code for Same Day Delivery.*'));
+        this.zipDeliveryInputField = page.getByTitle(new RegExp('.*homefield-textinput.*'));
+        this.zipDeliveryUpdateButton = page.getByLabel('Update');
+
+        // shipping filters
         this.pickupFilterButton = page.getByRole('button', { name: 'Pickup filter' });
+        this.shipFilterButtonReact = page.getByRole('button', { name: 'Ship filter' });
+        this.shipFilterButtonAngular = page.locator('[id="shipping-button"]');
+        this.filterChipsReact = page.locator('[class="filter-chip"]');
+        this.filterChipsAngular = page.locator('[class="hmf-global-chips-container"]');
+
+        // product attribute filters
+        this.sizeAccordionFilterButtonReact = page.locator('[class="rs-multi-select-facet rs-facet-wrapper rs-facet-wrapper-size"]');
+        this.sizeAccordionFilterButtonAngular = page.locator('[aria-controls="Size-accordion-panel"]');
+        this.saleAccordionFilterButtonReact = page.locator('[class="rs-multi-select-facet rs-facet-wrapper rs-facet-wrapper-sale"]');
+        this.saleAccordionFilterButtonAngular = page.locator('[aria-controls="Sale-accordion-panel"]');
+        this.sizeFilterValueReact = page.getByLabel('L',{ exact: true });
+        this.sizeFilterValueAngular = page.getByText('L', { exact: true });
+        this.saleFilterValueReact = page.locator('[class="mdc-switch  mdc-switch--disabled"]');
+        this.saleFilterValueAngular = page.locator('[class="checkbox-container]', {hasText: 'Sale'} );
+
+        // product attributes
         this.productNames = page.locator('[class="rs_product_description d-block"]');
+        this.productNamesAngular = page.locator('[class="product-title-link hmf-subheader-m hmf-header-m-xs hmf-mb-xxs hmf-mb-m-0"]');
+        this.productPromotionsReact = page.locator('[class="rs-promotions"]');
+        this.productPromotionsAngular = page.locator('[class="hmf-mb-xxs promo-message"]');
+        this.productAvailabilityReact = page.locator('[class="rs_circle_icon_hw"]');
+        this.productAvailabilityAngular = page.locator('[class="availability hmf-display-flex hmf-flex-wrap hmf-justify-content-between hmf-align-items-center hmf-align-content-center"]');
+        this.productPriceReact = page.locator('[class="rs_product_price"]');
+        this.productPriceAngular = page.locator('[class="price-text ng-star-inserted"]');
+
+        // pagination
+        this.rightChevronNextButtonReact = page.locator('[class="rs-size-chevron"]');
+        this.rightChevronNextButtonAngular = page.locator('[name="chevron-right"]');
+        this.highlightedPageNumberReact = page.locator('[class="active rs-page-item"]');
+        this.highlightedPageNumberAngular = page.locator('[class="bottom-pagination-number homefield-text-link ng-star-inserted selected"]');
+
+        // sorting options
+        this.sortOptionsAccordionButtonReact = page.locator('[class="rs-sort-opn-close-icon"]');
+        this.sortOptionsAccordionButtonAngular = page.getByTitle('Select sort option');
+        this.sortOptionsSelectionReact = page.locator('li');
+        this.sortOptionsSelectionAngular = page.locator('option');
+        this.sortSelectedReact = page.locator('[class="rs-selected-sort-text"]');
+        this.sortSelectedAngular = page.locator('[class="ng-star-inserted"]').locator('[selected="true"]');
+
+        // quickview
+        this.quickviewOpenATCButtonReact = page.locator('[class="dsg-quickview-button-icon"]');
+        this.quickviewOpenATCButtonAngular = page.locator('[id="add-to-cart-button"]');
+        this.quickviewColorAttribute = page.getByTitle('Black/Steel');
+        this.quickviewSizeAttribute = page.getByTestId('quickViewModalL');
+        this.quickviewModalATCButton = page.getByLabel('Add To Cart');
+        this.quickviewKeepShoppingButton = page.getByLabel('Keep Shopping');
+        this.quickviewViewCartButton = page.getByLabel('View Cart');
+
     }
 
     async setStoreFromPLP(store: string): Promise<string> {
@@ -70,5 +162,13 @@ export class ProductListingPage {
         await this.productNames.nth(indexOfProductFirstMatch).click();
 
         return productName;
+    }
+
+    async setDeliveryZipPLP(zip: string){
+        await this.page.waitForTimeout(4000); // waits for 4 seconds
+        await this.zipDeliveryLocationButton.click()
+        await this.zipDeliveryInputField.click()
+        await this.zipDeliveryInputField.fill(zip)
+        await this.zipDeliveryUpdateButton.click()
     }
 }
---
+++ test-data/smokePLPProdTestData.js
@@ -0,0 +1,17 @@
+export const testData_smokePLP_prod = {
+    searchTerm1: "mens polo",
+    productMatch1: "polo",
+    storeSearch: "Robinson",
+    email: "AutomationTestingDSG@gmail.com",
+    firstname: "Tiger",
+    lastName: "King",
+    phoneNumber: "72427334008",
+    address: "345 Court St. Coraopolis",
+    address2: "",
+    zipCode: "15108",
+    creditCardNumber: "4111 1111 1111 1111",
+    expiryDate: "03/30",
+    securityCode: "737",
+    usernameOrderAPI: "sean.clawson@dcsg.com",
+    passwordOrderAPI: "July@2021"
+}
