29080c597d0469e2ac7f2487b2304bedd31d79b0
maheshmancharlaDSG <133136492+maheshmancharlaDSG@users.noreply.github.com>
2024-08-08 11:59:21 -0400

    Added comment

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 4 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  /* added MS team report */
   reporter: [
     ['html'],
     [
