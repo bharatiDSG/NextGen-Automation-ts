ed0db62aa9aa7c1b81b7e5871a0986786e2372d8
maheshmancharlaDSG <133136492+maheshmancharlaDSG@users.noreply.github.com>
2024-08-08 17:22:49 -0400

    Updated github run number

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
         webhookUrl: `${process.env.WEB_HOOKS_URL}`,
         webhookType: "powerautomate", // or "msteams"
         linkToResultsUrl: `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`,
-        title:`${process.env.GITHUB_WORKFLOW}-${process.env.GITHUB_RUN_ID}`,
+        title:`${process.env.GITHUB_WORKFLOW}-${process.env.GITHUB_RUN_NUMBER}`,
         notifyOnSuccess: process.env.NOTIFY_ON_SUCCESS,
         mentionOnFailure: `${process.env.MENTIONS}`,
         mentionOnFailureText: "{mentions} check those failed tests!"
