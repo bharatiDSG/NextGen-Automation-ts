ede4bc176810069bd1738e86c2e7972d5b384346
Jake Jarosz <jacob.jarosz@dcsg.com>
2024-08-12 17:47:14 -0400

    trying notifyOnSuccess default

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
         webhookType: "powerautomate", // or "msteams"
         linkToResultsUrl: `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`,
         title:`${process.env.GITHUB_WORKFLOW}-${process.env.GITHUB_RUN_NUMBER}`,
-        notifyOnSuccess: process.env.NOTIFY_ON_SUCCESS,
+        notifyOnSuccess: process.env.NOTIFY_ON_SUCCESS === 'true',
         mentionOnFailure: `${process.env.MENTIONS}`,
         mentionOnFailureText: "{mentions} check those failed tests!"
       }
